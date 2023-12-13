d52ffbe4d4c6a8b5e90ee1201407a79fdff85962
Shahzada Fahad Ashraf <fahad.ashraf@whizzbridge.com>
2023-12-13 18:25:13 +0500

    first commit

---
+++ HelloToDotnet/Controllers/HelloController.cs
@@ -0,0 +1,43 @@
+﻿using HelloToDotnet.DTOs;
+using HelloToDotnet.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloToDotnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet("/get-hello")]
+        public string GetHelloWorld()
+        {
+            HelloService hs = new HelloService();
+            return "Hello World " + hs.GetDateTimeString();
+        }
+
+        // query string
+        // premitive types
+        // int, string, boolean, float, objects 🚫
+        [HttpGet("/get-hello/{id}/{name}")]
+        public string GetHelloWorld(string id, string name)
+        {
+            HelloService hs = new HelloService();
+            return "Hello World " + id + " " + name + hs.GetDateTimeString();
+        }
+
+        [HttpPost("/get-hello-post")]
+        public string GetHelloWorldPost([FromBody] Person person)
+        {
+            return "Hello World Post " + person.Name;
+        }
+
+        [HttpGet("/get-hello-list")]
+        public List<string> GetHelloWorldList()
+        {
+            return new List<string> {
+            "Hello",
+            "World"
+            };
+        }
+    }
+}
---
+++ HelloToDotnet/Services/HelloService.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloToDotnet.Services
+{
+    public class HelloService
+    {
+        public string GetDateTimeString()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("hello date time");
+            }
+            int j = 10;
+            int k = 0;
+            return DateTime.Now.ToLocalTime().ToString();
+        }
+    }
+}
